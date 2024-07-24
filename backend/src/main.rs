f7ae561c585350096e2a9df4e1b3e9bf6624c83d
Logan Norman <lognorman20@gmail.com>
2024-07-23 20:53:41 -0700

    add basic checks, add backend option, add asm route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -14,8 +14,10 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/check", get(check))
-        .route("/run", post(run));
+        .route("/check_files", get(check_files))
+        .route("/check_bin", get(check_bin))
+        .route("/run", post(run))
+        .route("/get_asm", post(get_asm));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("running app rn big bro");
@@ -26,20 +28,33 @@ async fn root() -> &'static str {
     "SPOTEMGOTEM"
 }
 
-async fn check() -> String {
+async fn check_files() -> String {
     let output = Command::new("ls")
         .output()
         .expect("Failed to execute command");
 
     if !output.status.success() {
-        eprintln!("Command executed with failing error code");
-        return "Failed to ls".to_string();
+        eprintln!("Failed to run `ls`");
     }
 
     String::from_utf8_lossy(&output.stdout).to_string()
 }
 
-async fn run(Json(payload): Json<File>) -> (StatusCode, String) {
+async fn check_bin() -> String {
+    let output = Command::new("noname")
+        .arg("-V")
+        .output()
+        .expect("Failed to execute command");
+
+    if !output.status.success() {
+        eprintln!("Failed to run `noname -V`");
+    }
+
+    String::from_utf8_lossy(&output.stdout).to_string()
+}
+
+// TODO: add option to print the asm
+async fn run(Json(payload): Json<ProgramInfo>) -> (StatusCode, String) {
     // create the source code file
     println!("CREATING SOURCE CODE FILE");
     if let Ok(mut file) = StdFile::create("tmp/src/main.no") {
@@ -101,28 +116,52 @@ async fn run(Json(payload): Json<File>) -> (StatusCode, String) {
         .args(&["--path", "tmp/src/main.no"])
         .args(&["--private-inputs", "tmp/private_input.json"])
         .args(&["--public-inputs", "tmp/public_input.json"])
+        .args(&["--backend", &payload.backend])
         .arg("--debug")
         .output()
         .expect("Failed to execute command");
 
-    if !output.status.success() {
-        eprintln!("Command executed with failing error code");
-        return (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "Failed to run code".to_string(),
-        );
-    }
-
     let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
 
     println!("successfully ran code, here's the response big bro");
 
-    (StatusCode::OK, stdout.to_string())
+    if output.status.success() {
+        // getting the asm
+        (StatusCode::OK, stdout.to_string())
+    } else {
+        (StatusCode::ACCEPTED, stderr.to_string())
+    }
+}
+
+async fn get_asm(Json(payload): Json<ProgramInfo>) -> (StatusCode, String) {
+    println!("GETTING ASM");
+    let output = Command::new("noname")
+        .arg("test")
+        .args(&["--path", "tmp/src/main.no"])
+        .args(&["--private-inputs", "tmp/private_input.json"])
+        .args(&["--public-inputs", "tmp/public_input.json"])
+        .args(&["--backend", &payload.backend])
+        .output()
+        .expect("Failed to execute command");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+
+    println!("successfully ran code, here's the response big bro");
+
+    if output.status.success() {
+        // getting the asm
+        (StatusCode::OK, stdout.to_string())
+    } else {
+        (StatusCode::ACCEPTED, stderr.to_string())
+    }
 }
 
 #[derive(Deserialize)]
-struct File {
+struct ProgramInfo {
     code: String,
     public_input: String,
     private_input: String,
+    backend: String,
 }
