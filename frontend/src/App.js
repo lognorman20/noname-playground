e03ca3fc26254848bcef9151941814481a7969ac
Logan Norman <lognorman20@gmail.com>
2024-07-24 21:35:41 -0700

    frontend looking decent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { Grid, Paper, Typography, TextareaAutosize } from "@mui/material";
+import {
+  Grid,
+  Paper,
+  Button,
+  Typography,
+  TextareaAutosize,
+} from "@mui/material";
 import CodeMirror from "@uiw/react-codemirror";
 import { rust, rustLanguage } from "@codemirror/lang-rust";
+import { json, jsonLanguage } from "@codemirror/lang-json";
 import { languages } from "@codemirror/language-data";
 import "./App.css";
 
@@ -50,9 +57,9 @@ function App() {
           Noname Code Playground
         </Typography>
       </header>
-      <Grid container spacing={2} className="main-content">
+      <Grid container spacing={2}>
         <Grid item xs={8}>
-          <Paper sx={{ bgcolor: "#1e1e1e" }} elevation={0}>
+          <Paper sx={{ bgcolor: "#21252d" }} elevation={0}>
             <CodeMirror
               value={codeValue}
               placeholder={"Please enter your Noname code here..."}
@@ -71,112 +78,149 @@ function App() {
           </Paper>
         </Grid>
         <Grid item xs={4}>
-          <Grid container spacing={2} direction={"column"}>
+          <Grid container direction={"column"} spacing={1}>
             <Grid item xs={4}>
-              <Paper sx={{ bgcolor: "#1e1e1e" }} elevation={0}>
-                <Typography
-                  align="center"
-                  variant="h5"
-                  sx={{ color: "#ffffff" }}
-                >
-                  Private Inputs
-                </Typography>
-                <CodeMirror
-                  value={privateInput}
-                  placeholder={"Private input here"}
-                  theme={"dark"}
-                  height="20vh"
-                  width="100%"
-                  basicSetup={basicSetupOptions}
-                  autoFocus={true}
-                  onChange={onPrivateInputChange}
-                  extensions={rust({
-                    base: rustLanguage,
-                    codeLanguages: languages,
-                  })}
-                  className="code-editor"
-                />
-              </Paper>
+              <Grid container direction={"column"} spacing={0}>
+                <Grid item xs={6}>
+                  <Typography
+                    align="center"
+                    variant="h5"
+                    sx={{ color: "#ffffff" }}
+                  >
+                    Public Inputs
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <CodeMirror
+                    value={privateInput}
+                    placeholder={"Private input here"}
+                    theme={"dark"}
+                    height="22vh"
+                    width="100%"
+                    basicSetup={basicSetupOptions}
+                    autoFocus={true}
+                    onChange={onPrivateInputChange}
+                    extensions={json({
+                      base: jsonLanguage,
+                      codeLanguages: languages,
+                    })}
+                    className="code-editor"
+                  />
+                </Grid>
+              </Grid>
             </Grid>
             <Grid item xs={4}>
-              <Paper sx={{ bgcolor: "#1e1e1e" }} elevation={0}>
-                <Typography
-                  align="center"
-                  variant="h5"
-                  sx={{ color: "#ffffff" }}
-                >
-                  Public Inputs
-                </Typography>
-                <CodeMirror
-                  value={publicInput}
-                  placeholder={"Public input here"}
-                  theme={"dark"}
-                  height="20vh"
-                  width="100%"
-                  basicSetup={basicSetupOptions}
-                  autoFocus={true}
-                  onChange={onPublicInputChange}
-                  extensions={rust({
-                    base: rustLanguage,
-                    codeLanguages: languages,
-                  })}
-                  className="code-editor"
-                />
-              </Paper>
+              <Grid container direction={"column"} spacing={0}>
+                <Grid item xs={6}>
+                  <Typography
+                    align="center"
+                    variant="h5"
+                    sx={{ color: "#ffffff" }}
+                  >
+                    Public Inputs
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <CodeMirror
+                    value={publicInput}
+                    placeholder={"Public input here"}
+                    theme={"dark"}
+                    height="22vh"
+                    width="100%"
+                    basicSetup={basicSetupOptions}
+                    autoFocus={true}
+                    onChange={onPublicInputChange}
+                    extensions={json({
+                      base: jsonLanguage,
+                      codeLanguages: languages,
+                    })}
+                    className="code-editor"
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={4}>
+              <Grid
+                container
+                direction={"row"}
+                spacing={3}
+                justifyContent="center"
+              >
+                <Grid item xs="auto">
+                  <Button
+                    sx={{ backgroundColor: "#292c34", color: "#ffffff" }}
+                    disableRipple
+                  >
+                    Run
+                  </Button>
+                </Grid>
+                <Grid item xs="auto">
+                  <Button
+                    sx={{ backgroundColor: "#292c34", color: "#ffffff" }}
+                    disableRipple
+                  >
+                    Generate Assembly
+                  </Button>
+                </Grid>
+                <Grid item xs="auto">
+                  <Button
+                    sx={{ backgroundColor: "#292c34", color: "#ffffff" }}
+                    disableRipple
+                  >
+                    Save
+                  </Button>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
         <Grid item xs={6}>
-          <Paper sx={{ bgcolor: "#1e1e1e" }}>
-            <Typography align="center" sx={{ color: "#ffffff" }} variant="h5">
-              Code Output
-            </Typography>
-            <TextareaAutosize
-              minRows={10}
-              maxRows={20}
-              value={compilationResult}
-              readOnly
-              placeholder="Compilation result will appear here..."
-              style={{
-                width: "100%",
-                backgroundColor: "#1e1e1e",
-                color: "#d4d4d4",
-                border: "1px solid #444",
-                borderRadius: "4px",
-                padding: "10px",
-                fontFamily: "monospace",
-                fontSize: "14px",
-                resize: "none",
-                boxSizing: "border-box",
-              }}
-            />
-          </Paper>
+          <Typography align="center" sx={{ color: "#ffffff" }} variant="h5">
+            Code Output
+          </Typography>
+          <TextareaAutosize
+            minRows={10}
+            maxRows={20}
+            value={compilationResult}
+            readOnly
+            placeholder="Compilation result will appear here..."
+            style={{
+              width: "100%",
+              backgroundColor: "#292c34",
+              color: "#d4d4d4",
+              border: "1px solid #444",
+              borderRadius: "4px",
+              padding: "10px",
+              fontFamily: "monospace",
+              fontSize: "14px",
+              resize: "none",
+              boxSizing: "border-box",
+            }}
+          />
         </Grid>
         <Grid item xs={6}>
-          <Paper sx={{ bgcolor: "#1e1e1e" }}>
-            <Typography align="center" sx={{ color: "#ffffff" }} variant="h5">
-              Assembly Output
-            </Typography>
-            <TextareaAutosize
-              minRows={10}
-              maxRows={20}
-              value={assemblyCode}
-              readOnly
-              placeholder="Assembly code will appear here..."
-              style={{
-                width: "100%",
-                backgroundColor: "#1e1e1e",
-                color: "#d4d4d4",
-                border: "1px solid #444",
-                borderRadius: "4px",
-                padding: "10px",
-                fontFamily: "monospace",
-                fontSize: "14px",
-                resize: "none",
-                boxSizing: "border-box",
-              }}
-            />
-          </Paper>
+          <Typography align="center" sx={{ color: "#ffffff" }} variant="h5">
+            Assembly Output
+          </Typography>
+          <TextareaAutosize
+            minRows={10}
+            maxRows={20}
+            value={assemblyCode}
+            readOnly
+            placeholder="Assembly code will appear here..."
+            style={{
+              width: "100%",
+              backgroundColor: "#292c34",
+              color: "#d4d4d4",
+              border: "1px solid #444",
+              borderRadius: "4px",
+              padding: "10px",
+              fontFamily: "monospace",
+              fontSize: "14px",
+              resize: "none",
+              boxSizing: "border-box",
+            }}
+          />
         </Grid>
       </Grid>
     </div>
